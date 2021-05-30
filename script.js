b66bda3b8c1ca4c31f883ce22b06617e91e5dec4
Hanady94 <>
2021-05-30 12:15:07 +0000

    my first webpage using javascript loop

---
+++ script.js
@@ -0,0 +1,31 @@
+var userName = prompt ('please enter your name');
+alert ('welcome to my webpage' + userName );
+var userAge = ('pleaes enter your Age');
+prompt ('please enter your age');
+
+var favPitAnimal = prompt ('please enter your fav pit animal chooes cats or dogs');
+
+while (favPitAnimal != 'cat' && userAge != 27)
+{
+  
+  userAge = ('pleaes enter your Age');
+  favPitAnimal = prompt ('please enter your fav pit animal chooes cats or dogs');
+   userAge = ('pleaes enter your Age');
+  favPitAnimal = prompt ('please enter your fav pit animal chooes cats or dogs');
+  break;
+}
+
+var numberOfFishs = prompt ('how many fishs you want?');
+
+for (var i = 0; i < numberOfFishs; i++ )
+{
+
+  document.write( '<img src="https://gameforge.com/de-DE/littlegames/includes/images/games/8581_5eb3edf2afb29.jpg"/>')
+}
+
+
+
+
+
+
+
